1c599cde10899e5bdc334f350a6ebd646ced7080
Admin <admin@Zuquitos-Mac.fibertel.com.ar>
2019-11-16 07:29:50 -0300

    numero 0

--- src/App.js
+++ src/App.js
@@ -10,14 +10,19 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <select>
+          <option value="grapefruit">Grapefruit</option>
+          <option value="lime">Lime</option>
+          <option selected value="coconut">Coconut</option>
+          <option value="mango">Mango</option>
+        </select>
+
+
+
+
+
+
       </header>
     </div>
   );
