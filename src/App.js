734a93d89e8c20129864a54d17a42707896895a8
Admin <admin@Zuquitos-Mac.fibertel.com.ar>
2019-11-16 11:17:11 -0300

    Agregado de Boostrap y armado basico de la wab responsive

--- src/App.js
+++ src/App.js
@@ -1,29 +1,101 @@
 import React from 'react';
+import { Button, Carousel,Card} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+ 
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <select>
-          <option value="grapefruit">Grapefruit</option>
-          <option value="lime">Lime</option>
-          <option selected value="coconut">Coconut</option>
-          <option value="mango">Mango</option>
-        </select>
-
-
-
-
-
-
+      <p>POD es una app que te ayuda a utilizar tus tarjetas en el local correcto el dia correcto</p>
+      <p>para optimizar al maximo tu dinero!</p>
+      <p>dssd</p>
+       
       </header>
+      
+      <body>
+
+      <div>
+        <Carousel className='App-myCarousel'>
+        <Carousel.Item>
+          <div className='App-myCard'>
+          <div class="card" >
+            <div class="card-body">
+              <Card.Title class="text-primary">Card Title</Card.Title>
+                  <Card.Text class="text-dark">
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </div>
+          </div>
+        </div>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <div className='App-myCard'>
+        <div class="card">
+          <div class="card-body">
+            <Card.Title class="text-primary">Card Title este es el 2do</Card.Title>
+                <Card.Text class="text-dark">
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+          </div>
+        </div>
+        </div>
+        </Carousel.Item>
+
+        <Carousel.Item>
+        <div className='App-myCard'>
+        <div class="card">
+          <div class="card-body">
+            <Card.Title class="text-primary">Card Title este es el 3ero</Card.Title>
+                <Card.Text class="text-dark">
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </div>
+          </div>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+
+        </div>
+
+        <form>
+          <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
+          </div>
+            <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
+          </div>
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+             <span class="input-group-text" id="basic-addon1">Last name:</span>
+            </div>
+          <input type="text" class="form-control" placeholder="" aria-label="Nombre" aria-describedby="basic-addon1"></input>
+        </div>
+
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" placeholder="Username" id="inputGroup-sizing-default">@</span>
+          </div>
+            <input type="Email" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
+          </div>
+
+
+          <Button variant="primary" type="submit">Primary</Button>
+
+        </form>
+      </body>
     </div>
   );
 }
